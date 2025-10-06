5fc50df8d7e1ce2388def4440c3b92720696f5d7
tle130475c <tle130475c@gmail.com>
2025-10-06 18:32:09 +0700

    add full system install script

--- install_archlinux.sh
+++ install_archlinux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
 root_password="<root_password>"
 username="<username>"
@@ -8,17 +8,14 @@ realname="<realname>"
 user_password="<user_password>"
 hostname="<hostname>"
 
-wifi_ssid="<wifi_ssid>"
-wifi_password="<wifi_password>"
-
+disk_name="<disk_name>"
+partition_name="<partition_name>"
 luks_password="<luks_password>"
 
-# Connect to Wifi
-ping -c 3 8.8.8.8
-if [ $? -ne 0 ]; then
-    iwctl --passphrase="$wifi_password" station wlan0 connect "$wifi_ssid"
-    sleep 10
-fi
+run_command_as_user() {
+    local command="$1"
+    arch-chroot -u "$username" /mnt bash -c "export HOME=/home/$username && $command"
+}
 
 # Waiting for keyring to be initialized
 systemctl start archlinux-keyring-wkd-sync.timer
@@ -37,34 +34,34 @@ printf "Server = https://arch-mirror.wtako.net/\$repo/os/\$arch\n" >> /etc/pacma
 printf "Server = https://mirror-hk.koddos.net/archlinux/\$repo/os/\$arch\n" >> /etc/pacman.d/mirrorlist
 
 # Create partitions
-sgdisk -Z /dev/nvme0n1
-wipefs -a /dev/nvme0n1
-sgdisk -n 0:0:+550M -t 0:ef00 -c 0:esp /dev/nvme0n1
-wipefs -a /dev/nvme0n1p1
-sgdisk -n 0:0:+550M -t 0:ea00 -c 0:XBOOTLDR /dev/nvme0n1
-wipefs -a /dev/nvme0n1p2
-sgdisk -n 0:0:0 -t 0:8309 -c 0:luks-encrypted /dev/nvme0n1
-wipefs -a /dev/nvme0n1p3
-printf "$luks_password" | cryptsetup luksFormat --type luks2 /dev/nvme0n1p3 -
-printf "$luks_password" | cryptsetup open /dev/nvme0n1p3 encrypt-lvm -
+sgdisk -Z /dev/$disk_name
+wipefs -a /dev/$disk_name
+sgdisk -n 0:0:+550M -t 0:ef00 -c 0:esp /dev/$disk_name
+wipefs -a /dev/${partition_name}1
+sgdisk -n 0:0:+550M -t 0:ea00 -c 0:XBOOTLDR /dev/$disk_name
+wipefs -a /dev/${partition_name}2
+sgdisk -n 0:0:0 -t 0:8309 -c 0:luks-encrypted /dev/$disk_name
+wipefs -a /dev/${partition_name}3
+printf "$luks_password" | cryptsetup luksFormat --type luks2 /dev/${partition_name}3 -
+printf "$luks_password" | cryptsetup open /dev/${partition_name}3 encrypt-lvm -
 wipefs -a /dev/mapper/encrypt-lvm
 pvcreate /dev/mapper/encrypt-lvm
 vgcreate vg-system /dev/mapper/encrypt-lvm
 lvcreate -L 48G vg-system -n swap
 lvcreate -l +100%FREE vg-system -n root
-mkfs.vfat -F32 /dev/nvme0n1p1
-mkfs.vfat -F32 /dev/nvme0n1p2
+mkfs.vfat -F32 /dev/${partition_name}1
+mkfs.vfat -F32 /dev/${partition_name}2
 mkswap /dev/vg-system/swap
 mkfs.ext4 /dev/vg-system/root
 mount /dev/vg-system/root /mnt
 mkdir -p /mnt/efi
-mount /dev/nvme0n1p1 /mnt/efi
+mount /dev/${partition_name}1 /mnt/efi
 mkdir -p /mnt/boot
-mount /dev/nvme0n1p2 /mnt/boot
+mount /dev/${partition_name}2 /mnt/boot
 swapon /dev/vg-system/swap
 
 # Install essential packages
-pacstrap /mnt base base-devel linux linux-headers linux-firmware man-pages man-db iptables-nft pipewire pipewire-pulse pipewire-alsa alsa-utils gst-plugin-pipewire wireplumber bash-completion nfs-utils gvim linux-lts linux-lts-headers
+pacstrap /mnt base base-devel linux linux-headers linux-firmware man-pages man-db iptables-nft pipewire pipewire-pulse pipewire-alsa alsa-utils gst-plugin-pipewire wireplumber bash-completion nfs-utils gvim
 
 # Disable makepkg debug
 linum=$(arch-chroot /mnt sed -n "/^OPTIONS=(.*)$/=" /etc/makepkg.conf)
@@ -111,6 +108,10 @@ printf "Defaults passwd_timeout=0\n" >> /mnt/etc/sudoers
 printf "\n## Disable sudo timestamp timeout\n" >> /mnt/etc/sudoers
 printf "Defaults timestamp_timeout=-1\n" >> /mnt/etc/sudoers
 
+# Allow members of wheel group to execute any command
+linum=$(arch-chroot /mnt sed -n "/^# %wheel ALL=(ALL:ALL) ALL$/=" /etc/sudoers)
+arch-chroot /mnt sed -i "${linum}s/^# //" /etc/sudoers
+
 # Configure mkinitcpio
 arch-chroot /mnt pacman -Syu --needed --noconfirm lvm2
 linum=$(arch-chroot /mnt sed -n "/^HOOKS=(.*)$/=" /etc/mkinitcpio.conf)
@@ -133,24 +134,7 @@ printf "title Arch Linux\n " > $boot_entry_filename
 printf "linux /vmlinuz-linux\n" >> $boot_entry_filename
 printf "initrd /intel-ucode.img\n" >> $boot_entry_filename
 printf "initrd /initramfs-linux.img\n" >> $boot_entry_filename
-printf "options cryptdevice=UUID=$(blkid -s UUID -o value /dev/nvme0n1p3):encrypt-lvm root=/dev/vg-system/root resume=UUID=$(blkid -s UUID -o value /dev/vg-system/swap) rw\n" >> $boot_entry_filename
-
-# Temporarily allow members of wheel group to execute any command without password
-linum=$(arch-chroot /mnt sed -n "/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL$/=" /etc/sudoers)
-arch-chroot /mnt sed -i "${linum}s/^# //" /etc/sudoers
-
-# Install Yay AUR helper
-arch-chroot /mnt pacman -Syu --needed --noconfirm go
-
-run_command_as_user() {
-    local command="$1"
-    arch-chroot -u "$username" /mnt bash -c "export HOME=/home/$username && $command"
-}
-
-run_command_as_user "mkdir /home/$username/tmp"
-run_command_as_user "curl -LJo /home/$username/tmp/yay.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz"
-run_command_as_user "tar -xvf /home/$username/tmp/yay.tar.gz -C /home/$username/tmp"
-run_command_as_user "export GOCACHE='/home/$username/.cache/go-build' && cd /home/$username/tmp/yay && makepkg -sri --noconfirm"
+printf "options cryptdevice=UUID=$(blkid -s UUID -o value /dev/${partition_name}3):encrypt-lvm root=/dev/vg-system/root resume=UUID=$(blkid -s UUID -o value /dev/vg-system/swap) rw\n" >> $boot_entry_filename
 
 # Install KVM
 arch-chroot /mnt pacman -Syu --needed --noconfirm virt-manager qemu-full vde2 dnsmasq bridge-utils virt-viewer dmidecode edk2-ovmf iptables-nft swtpm qemu-hw-usb-host qemu-block-gluster qemu-block-iscsi
@@ -163,8 +147,8 @@ arch-chroot /mnt sed -i "${linum}s/^#//" $libvirtd_conf_file
 linum=$(arch-chroot /mnt sed -n "/^#unix_sock_rw_perms = \"0770\"$/=" $libvirtd_conf_file)
 arch-chroot /mnt sed -i "${linum}s/^#//" $libvirtd_conf_file
 
-arch-chroot /mnt usermod -a -G libvirt $username
-arch-chroot /mnt usermod -a -G kvm $username
+arch-chroot /mnt usermod -aG libvirt $username
+arch-chroot /mnt usermod -aG kvm $username
 
 # Install Intel packages
 arch-chroot /mnt pacman -Syu --needed --noconfirm mesa lib32-mesa ocl-icd lib32-ocl-icd intel-compute-runtime vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader intel-media-driver vpl-gpu-rt libva-utils intel-media-sdk sof-firmware
@@ -177,15 +161,59 @@ arch-chroot /mnt pacman -Syu --needed --noconfirm thermald
 arch-chroot /mnt systemctl enable thermald.service
 
 # Install GNOME Desktop Environment
-arch-chroot /mnt pacman -Syu --needed --noconfirm xorg-server baobab eog evince file-roller gdm gnome-calculator gnome-calendar gnome-clocks gnome-color-manager gnome-control-center gnome-font-viewer gnome-keyring gnome-screenshot gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra gnome-video-effects nautilus sushi gnome-tweaks totem xdg-user-dirs-gtk gnome-usage endeavour dconf-editor gnome-shell-extension-appindicator alacarte gnome-text-editor gnome-sound-recorder seahorse seahorse-nautilus gnome-browser-connector xdg-desktop-portal xdg-desktop-portal-gnome gnome-disk-utility libappindicator-gtk3 transmission-gtk power-profiles-daemon gvfs-smb gvfs-google gvfs-mtp gvfs-nfs gnome-logs evolution evolution-ews evolution-on gnome-software gnome-boxes gnome-remote-desktop gnome-connections gedit gedit-plugins
+arch-chroot /mnt pacman -Syu --needed --noconfirm xorg-server baobab eog evince file-roller gdm gnome-calculator gnome-calendar gnome-clocks gnome-color-manager gnome-control-center gnome-font-viewer gnome-keyring gnome-screenshot gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra gnome-video-effects nautilus sushi gnome-tweaks totem xdg-user-dirs-gtk gnome-usage endeavour dconf-editor gnome-shell-extension-appindicator alacarte gnome-text-editor gnome-sound-recorder seahorse seahorse-nautilus gnome-browser-connector xdg-desktop-portal xdg-desktop-portal-gnome gnome-disk-utility libappindicator-gtk3 transmission-gtk power-profiles-daemon gvfs-smb gvfs-google gvfs-mtp gvfs-nfs gnome-logs evolution evolution-ews evolution-on gnome-software gnome-boxes gnome-remote-desktop gnome-connections gedit gedit-plugins gnome-characters
 arch-chroot /mnt systemctl enable gdm.service
 arch-chroot /mnt systemctl enable bluetooth.service
 run_command_as_user "mkdir -p /home/$username/.config/environment.d"
 
+# Temporarily allow members of wheel group to execute any command without password
+linum=$(arch-chroot /mnt sed -n "/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL$/=" /etc/sudoers)
+arch-chroot /mnt sed -i "${linum}s/^# //" /etc/sudoers
+
+# Install Yay AUR helper
+arch-chroot /mnt pacman -Syu --needed --noconfirm go
+run_command_as_user "mkdir /home/$username/tmp"
+run_command_as_user "curl -LJo /home/$username/tmp/yay.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz"
+run_command_as_user "tar -xvf /home/$username/tmp/yay.tar.gz -C /home/$username/tmp"
+run_command_as_user "export GOCACHE='/home/$username/.cache/go-build' && cd /home/$username/tmp/yay && makepkg -sri --noconfirm"
+
+# Fonts
+arch-chroot /mnt pacman -Syu --needed --noconfirm ttf-dejavu ttf-liberation noto-fonts-emoji ttf-cascadia-code ttf-fira-code ttf-roboto-mono ttf-hack noto-fonts-cjk
+
+# Web browsers
+arch-chroot /mnt pacman -Syu --needed --noconfirm torbrowser-launcher firefox-developer-edition chromium
+run_command_as_user "yay -Syu --needed --noconfirm vdhcoapp"
+
+# Tools
+arch-chroot /mnt pacman -Syu --needed --noconfirm keepassxc expect pacman-contrib dosfstools p7zip unarchiver bash-completion flatpak tree archiso rclone rsync lm_sensors ntfs-3g gparted exfatprogs pdftk filezilla texlive texlive-lang krusader gptfdisk kio5-extras smartmontools ddcutil proton-vpn-gtk-app libreoffice-fresh calibre kolourpaint vlc vlc-plugins-all gst-libav gst-plugins-good gst-plugins-ugly gst-plugins-bad obs-studio inkscape gimp kdenlive frei0r-plugins
+run_command_as_user "yay -Syu --needed --noconfirm dislocker ventoy-bin"
+
+# Remote desktop
+arch-chroot /mnt pacman -Syu --needed --noconfirm remmina freerdp
+
+# Programming tools
+arch-chroot /mnt pacman -Syu --needed --noconfirm git github-cli git-lfs kdiff3 valgrind kruler emacs-wayland bash-language-server azcopy azure-cli
+run_command_as_user "yay -Syu --needed --noconfirm visual-studio-code-bin openrefine storageexplorer"
+
+# Docker
+arch-chroot /mnt pacman -Syu --needed --noconfirm docker docker-compose docker-buildx minikube kubectl helm
+
+# Java
+arch-chroot /mnt pacman -Syu --needed --noconfirm jdk-openjdk openjdk-doc openjdk-src maven
+
+# Python
+arch-chroot /mnt pacman -Syu --needed --noconfirm python uv
+
+# JavaScript
+arch-chroot /mnt pacman -Syu --needed --noconfirm nvm eslint prettier
+run_command_as_user "printf '\n## nvm configuration\n' >> /home/$username/.bashrc"
+run_command_as_user "printf 'source /usr/share/nvm/init-nvm.sh\n' >> /home/$username/.bashrc"
+
+# VirtualBox
+arch-chroot /mnt pacman -Syu --needed --noconfirm virtualbox virtualbox-guest-iso virtualbox-host-dkms
+arch-chroot /mnt usermod -aG vboxusers $username
+run_command_as_user "yay -Syu --needed --noconfirm virtualbox-ext-oracle"
+
 # Disallow members of wheel group to execute any command without password
 linum=$(arch-chroot /mnt sed -n "/^%wheel ALL=(ALL:ALL) NOPASSWD: ALL$/=" /etc/sudoers)
 arch-chroot /mnt sed -i "${linum}s/^/# /" /etc/sudoers
-
-# Allow members of wheel group to execute any command
-linum=$(arch-chroot /mnt sed -n "/^# %wheel ALL=(ALL:ALL) ALL$/=" /etc/sudoers)
-arch-chroot /mnt sed -i "${linum}s/^# //" /etc/sudoers
