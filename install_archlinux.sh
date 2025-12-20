ab9cae4344d799beb678d6ef974eca82204ae8c2
tle130475c <tle130475c@gmail.com>
2025-12-20 15:40:55 +0700

    just install the default latest java

--- install_archlinux.sh
+++ install_archlinux.sh
@@ -196,9 +196,8 @@ run_command_as_user "yay -Syu --needed --noconfirm visual-studio-code-bin openre
 arch-chroot /mnt pacman -Syu --needed --noconfirm docker docker-compose docker-buildx minikube kubectl helm
 
 # Java
-arch-chroot /mnt pacman -Syu --needed --noconfirm jdk-openjdk openjdk-doc openjdk-src maven gradle gradle-doc jdk21-openjdk
+arch-chroot /mnt pacman -Syu --needed --noconfirm jdk-openjdk openjdk-doc openjdk-src maven gradle gradle-doc
 run_command_as_user "yay -Syu --needed --noconfirm jetbrains-toolbox"
-arch-chroot /mnt archlinux-java set java-21-openjdk
 
 # Python
 arch-chroot /mnt pacman -Syu --needed --noconfirm python uv
