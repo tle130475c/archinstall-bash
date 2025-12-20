f49760ffa6c2bb837d62861a8217ef92255d8251
tle130475c <tle130475c@gmail.com>
2025-12-20 11:45:51 +0700

    add some variables

--- system_info.sh
+++ system_info.sh
@@ -9,6 +9,10 @@ disk_name="<disk_name>"
 partition_name="<partition_name>"
 luks_password="<luks_password>"
 
+esp_size="1GiB"
+xbootlrd_size="1GiB"
+swap_size="<swap_size>"
+
 root_password="<root_password>"
 username="<username>"
 realname="<realname>"
