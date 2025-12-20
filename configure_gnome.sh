1952addc85f27711f0cb0425724026be11e9db57
tle130475c <tle130475c@gmail.com>
2025-12-20 11:44:25 +0700

    remove set gnome dark theme

--- configure_gnome.sh
+++ configure_gnome.sh
@@ -8,7 +8,6 @@ gsettings set org.gnome.desktop.interface font-antialiasing rgba
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.desktop.interface clock-format 24h
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gnome.nautilus.preferences default-folder-viewer list-view
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type nothing
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type nothing
