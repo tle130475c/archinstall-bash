121bf67626f69467f49e18e4c2f2c1d339e10363
tle130475c <tle130475c@gmail.com>
2025-12-25 09:10:38 +0700

    re-organize code

--- install_archlinux.sh
+++ install_archlinux.sh
@@ -25,26 +25,39 @@ printf "Server = https://mirror.xtom.com.hk/archlinux/\$repo/os/\$arch\n" > /etc
 printf "Server = https://arch-mirror.wtako.net/\$repo/os/\$arch\n" >> /etc/pacman.d/mirrorlist
 printf "Server = https://mirror-hk.koddos.net/archlinux/\$repo/os/\$arch\n" >> /etc/pacman.d/mirrorlist
 
-# Create partitions
+## Create partitions
+# Wipe the disk
 sgdisk -Z /dev/$disk_name
 wipefs -a /dev/$disk_name
+
+# Create ESP partition
 sgdisk -n 0:0:+$esp_size -t 0:ef00 -c 0:esp /dev/$disk_name
 wipefs -a /dev/${partition_name}1
+
+# Create XBOOTLDR partition
 sgdisk -n 0:0:+$xbootlrd_size -t 0:ea00 -c 0:XBOOTLDR /dev/$disk_name
 wipefs -a /dev/${partition_name}2
+
+# Create LUKS encrypted partition
 sgdisk -n 0:0:0 -t 0:8309 -c 0:luks-encrypted /dev/$disk_name
 wipefs -a /dev/${partition_name}3
 printf "$luks_password" | cryptsetup luksFormat --type luks2 /dev/${partition_name}3 -
+
+# Create LVM inside LUKS
 printf "$luks_password" | cryptsetup open /dev/${partition_name}3 encrypt-lvm -
 wipefs -a /dev/mapper/encrypt-lvm
 pvcreate /dev/mapper/encrypt-lvm
 vgcreate vg-system /dev/mapper/encrypt-lvm
 lvcreate -L $swap_size vg-system -n swap
 lvcreate -l +100%FREE vg-system -n root
+
+# Format partitions
 mkfs.vfat -F32 /dev/${partition_name}1
 mkfs.vfat -F32 /dev/${partition_name}2
 mkswap /dev/vg-system/swap
 mkfs.ext4 /dev/vg-system/root
+
+# Mount partitions
 mount /dev/vg-system/root /mnt
 mkdir -p /mnt/efi
 mount /dev/${partition_name}1 /mnt/efi
