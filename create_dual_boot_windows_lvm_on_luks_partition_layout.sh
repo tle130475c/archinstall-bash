cde1ca247be14b176c775ce1ec1f340e9268f4dd
tle130475c <tle130475c@gmail.com>
2025-12-27 18:24:07 +0700

    add script to create dual boot windows lvm on luks partition layout

---
+++ create_dual_boot_windows_lvm_on_luks_partition_layout.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source ./system_info.sh
+
+# Create XBOOTLDR partition
+sgdisk -n 0:0:+$xbootlrd_size -t 0:ea00 -c 0:XBOOTLDR /dev/$disk_name
+wipefs -a /dev/${partition_name}5
+
+# Create LUKS encrypted partition
+sgdisk -n 0:0:0 -t 0:8309 -c 0:luks-encrypted /dev/$disk_name
+wipefs -a /dev/${partition_name}6
+printf "$luks_password" | cryptsetup luksFormat --type luks2 /dev/${partition_name}6 -
+
+# Create LVM inside LUKS
+printf "$luks_password" | cryptsetup open /dev/${partition_name}6 encrypt-lvm -
+wipefs -a /dev/mapper/encrypt-lvm
+pvcreate /dev/mapper/encrypt-lvm
+vgcreate vg-system /dev/mapper/encrypt-lvm
+lvcreate -L $swap_size vg-system -n swap
+lvcreate -l +100%FREE vg-system -n root
+
+# Format partitions
+mkfs.vfat -F32 /dev/${partition_name}5
+mkswap /dev/vg-system/swap
+mkfs.ext4 /dev/vg-system/root
+
+# Mount partitions
+mount /dev/vg-system/root /mnt
+mkdir -p /mnt/efi
+mount /dev/${partition_name}1 /mnt/efi
+mkdir -p /mnt/boot
+mount /dev/${partition_name}5 /mnt/boot
+swapon /dev/vg-system/swap
